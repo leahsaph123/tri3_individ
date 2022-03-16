99f79a23165964f4b47d8b52689bae8d99ca07ad
leahsaph123 <leahbogomolny@gmail.com>
2022-03-16 11:44:46 -0700

    week 1 code- fibonacci and lists and loops

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+n = int(input("number of terms? "))
+while (n < 0 or n > 50):
+    n = int(input("please provide a valid input: "))
+
+def fibonacci(number):
+    if number <= 1:
+        return number
+    else:
+        return(fibonacci(number - 1) + fibonacci(number - 2))
+
+for index in range(n):
+    print(fibonacci(index))
---
+++ listsandloops.py
@@ -0,0 +1,58 @@
+list = []
+
+list.append({
+        "name": "Leah",
+        "grade": "12",
+        "group mates": ["Jessie", "Allie", "Michael"]
+    })
+
+list.append({
+    "name": "Jessie",
+    "grade": "12",
+    "group mates": ["Leah", "Allie", "Michael"]
+})
+
+list.append({
+    "name": "Allie",
+    "grade": "11",
+    "group mates": ["Leah", "Jessie", "Michael"]
+})
+
+list.append({
+    "name": "Michael",
+    "grade": "11",
+    "group mates": ["Leah", "Jessie", "Allie"]
+})
+
+def for_loop():
+    for entry in range(len(list)):
+        print(list[entry])
+
+def while_loop():
+    entry = 0
+    while (entry < len(list)):
+        print(list[entry])
+        entry += 1
+
+def recursive_loop(entry):
+    if entry < len(list):
+        print(list[entry])
+        recursive_loop(entry + 1)
+    return
+
+
+def run_loops():
+    option = input("would you like to run a for, while, or recursive loop? Please answer with a 'for', 'while', or 'recursive': ")
+
+    while not ((option == "for") or (option == "while") or (option == "recursive")):
+        option = input("please give a valid input: ")
+
+    if (option == "for"):
+        for_loop()
+    elif (option == "while"):
+        while_loop()
+    elif (option == "recursive"):
+        recursive_loop(entry=0)
+
+run_loops()
+
